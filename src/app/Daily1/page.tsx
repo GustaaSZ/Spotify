aad7b71612b54bbbe9c3386eaff13275396cb9a8
GustaaSz <gugaabrantes2015@gmail.com>
2024-10-11 18:33:22 -0300

    feat: adding navigation to the chevron left button

--- src/app/Daily1/page.tsx
+++ src/app/Daily1/page.tsx
@@ -2,78 +2,84 @@
 import { ChevronLeft, ChevronRight, Play } from "lucide-react";
 import Image from "next/image";
 import { ComponentSidebar } from "@/components/ComponentSidebar";
-import { ComponentMain } from "@/components/ComponentMain";
 import { ComponentFooter } from "@/components/ComponentFooter";
-import { PlayButtonMusics } from "@/components/PlayButtonMusics";
-import { PlayButtonPlaylists } from "@/components/PlayButtonPlaylists";
+import { useRouter } from "next/navigation";
 
 export default function Daily1() {
-    return(
-    
-      // Conteiner Principal
-      <div className="h-screen flex flex-col">
 
-        {/* // Conteiner que tem a SIDEBAR (espécie de menu lateral) e o corpo MAIN */}
-        <div className="flex flex-1">
+  const router = useRouter(); // Hook de navegação
 
-          {/* CHAMANDO O COMPONENTE SIDEBAR */}
-          <ComponentSidebar/>
+  // Função para navegar para a página inicial
+  const navigateHome = () => {
+    router.push("/"); // Caminho para a página principal, que renderiza `page.tsx`
+  };
+  
+  return(
+  
+    // Conteiner Principal
+    <div className="h-screen flex flex-col">
 
-          {/* MAIN */}
-          <main className="flex-1 p-7">
+      {/* // Conteiner que tem a SIDEBAR (espécie de menu lateral) e o corpo MAIN */}
+      <div className="flex flex-1">
 
-            {/* div para os botões superiores */}
-            <div className="flex items-center gap-6 ml-1">
-              <button className="rounded-full bg-black/40 p-1 hover:bg-zinc-700">
-                <ChevronLeft />
-              </button>
-              <button className="rounded-full bg-black/40 p-1 hover:bg-zinc-700">
-                <ChevronRight />
-              </button>
-            </div>
+        {/* CHAMANDO O COMPONENTE SIDEBAR */}
+        <ComponentSidebar/>
 
-            {/* Texto para a SEGUNDA parte da main */}
-            <h1 className="font-semibold text-3xl mt-10 ml-1 text-zinc-100 hover:underline">Daily Mix 1 - Lana Del Rey</h1>
+        {/* MAIN */}
+        <main className="flex-1 p-7">
 
-            {/* Div para a segunda parte da main */}
-            <div className="grid grid-cols-3 gap-6 mt-8">
-            
-              <nav className="mt-6 pt-10 border-t-2 border-zinc-700 space-y-4 flex flex-col scroll">
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">1 - Summertime Sadness</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">3 - Young and Beautiful</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">4 - Video Games</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">5 - Radio</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">6 - Burn to die</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">7 - Say Yes to Heaven</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">8 - Margaret</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">9 - Blue Jeans</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">10 - West Coast</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">11 - Love</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">12 - High by the beach</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">13 - Lust for life</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">14 - Art Deco</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">15 - Doin' Time</a>
-                <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">16 - Sad Girl</a>
-              </nav>
+          {/* div para os botões superiores */}
+          <div className="flex items-center gap-6 ml-1">
+            <button onClick={navigateHome} className="rounded-full bg-black/40 p-1 hover:bg-zinc-700">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1 hover:bg-zinc-700">
+              <ChevronRight />
+            </button>
+          </div>
 
-              
-              <a href="/Daily1" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3 w-full mt-14">
-                <Image src="/Lana.jpg" className="w-full rounded-lg mt-10" width={400} height={400} alt="Capa da playist da Lana del rey" />
-                
-                <button className="p-3 rounded-full bg-green-500 text-zinc-950 absolute bottom-24 ml-80 invisible group-hover:visible">
-                  <Play className="fill-transparent group-hover:fill-black"/>
-                </button>
-              </a>
+          {/* Texto para a SEGUNDA parte da main */}
+          <h1 className="font-semibold text-3xl mt-10 ml-1 text-zinc-100 hover:underline">Daily Mix 1 - Lana Del Rey</h1>
 
-            </div>
-
-          </main>
+          {/* Div para a segunda parte da main */}
+          <div className="grid grid-cols-3 gap-6 mt-8">
           
-        </div>
+            <nav className="mt-6 pt-10 border-t-2 border-zinc-700 space-y-4 flex flex-col scroll">
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">1 - Summertime Sadness</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">3 - Young and Beautiful</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">4 - Video Games</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">5 - Radio</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">6 - Burn to die</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">7 - Say Yes to Heaven</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">8 - Margaret</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">9 - Blue Jeans</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">10 - West Coast</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">11 - Love</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">12 - High by the beach</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">13 - Lust for life</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">14 - Art Deco</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">15 - Doin' Time</a>
+              <a href="" className="text-lg  text-zinc-400 hover:text-zinc-200 hover:font-semibold">16 - Sad Girl</a>
+            </nav>
 
-        {/* CHAMANDO O COMPONENTE FOOTER */}
-        <ComponentFooter />
+            
+            <a href="/Daily1" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3 w-full mt-14">
+              <Image src="/Lana.jpg" className="w-full rounded-lg mt-10" width={400} height={400} alt="Capa da playist da Lana del rey" />
+              
+              <button className="p-3 rounded-full bg-green-500 text-zinc-950 absolute bottom-24 ml-80 invisible group-hover:visible">
+                <Play className="fill-transparent group-hover:fill-black"/>
+              </button>
+            </a>
 
+          </div>
+
+        </main>
+        
       </div>
-    );
+
+      {/* CHAMANDO O COMPONENTE FOOTER */}
+      <ComponentFooter />
+
+    </div>
+  );
 }
--- src/components/ComponentMain.tsx
+++ src/components/ComponentMain.tsx
@@ -1,3 +1,4 @@
+'use client'; // Marcar como componente do lado do cliente
 import { ChevronLeft, ChevronRight, Play } from "lucide-react";
 import Image from "next/image";
 import { PlayButtonPlaylists } from "./PlayButtonPlaylists";
@@ -5,123 +6,123 @@ import { PlayButtonMusics } from "./PlayButtonMusics";
 
 export function ComponentMain() {
 
-    return(
-    
-        <main className="flex-1 p-7">
+  return(
+  
+      <main className="flex-1 p-7">
 
-          {/* div para os botões superiores */}
-          <div className="flex items-center gap-6 ml-1">
-            <button className="rounded-full bg-black/40 p-1 hover:bg-zinc-700">
-              <ChevronLeft />
-            </button>
-            <button className="rounded-full bg-black/40 p-1 hover:bg-zinc-700">
-              <ChevronRight />
-            </button>
-          </div>
+        {/* div para os botões superiores */}
+        <div className="flex items-center gap-6 ml-1">
+          <button className="rounded-full bg-black/40 p-1 hover:bg-zinc-700">
+            <ChevronLeft />
+          </button>
+          <button className="rounded-full bg-black/40 p-1 hover:bg-zinc-700">
+            <ChevronRight />
+          </button>
+        </div>
 
-          {/* Texto para a primeira parte da main */}
-          <h1 className="font-semibold text-3xl mt-10 ml-1 text-zinc-100">Good Afternoon</h1>
+        {/* Texto para a primeira parte da main */}
+        <h1 className="font-semibold text-3xl mt-10 ml-1 text-zinc-100">Good Afternoon</h1>
 
-          {/* Div com as colunas para colocar as musicas */}
-          <div className="grid grid-cols-3 gap-6 mt-5">
+        {/* Div com as colunas para colocar as musicas */}
+        <div className="grid grid-cols-3 gap-6 mt-5">
 
-            <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
-              <Image src="/album.jpeg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
-              <strong>Deus e eu no Sertão</strong>
+          <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
+            <Image src="/album.jpeg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
+            <strong>Deus e eu no Sertão</strong>
 
-              {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
-              <PlayButtonMusics />
-            </a>
-            <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
-              <Image src="/u2.jpg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
-              <strong>The best of 1980-1990</strong>
+            {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
+            <PlayButtonMusics />
+          </a>
+          <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
+            <Image src="/u2.jpg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
+            <strong>The best of 1980-1990</strong>
 
-              {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
-              <PlayButtonMusics />
-            </a>
-            <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
-              <Image src="/adele.jpg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
-              <strong>Adele 25</strong>
+            {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
+            <PlayButtonMusics />
+          </a>
+          <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
+            <Image src="/adele.jpg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
+            <strong>Adele 25</strong>
 
-              {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
-              <PlayButtonMusics />
-            </a>
-            <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
-              <Image src="/justin.jpg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
-              <strong>Purpose</strong>
+            {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
+            <PlayButtonMusics />
+          </a>
+          <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
+            <Image src="/justin.jpg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
+            <strong>Purpose</strong>
 
-              {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
-              <PlayButtonMusics />
-            </a>
-            <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
-              <Image src="/marilia.jpg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
-              <strong>Decretos Reais</strong>
+            {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
+            <PlayButtonMusics />
+          </a>
+          <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
+            <Image src="/marilia.jpg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
+            <strong>Decretos Reais</strong>
 
-              {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
-              <PlayButtonMusics />
-            </a>
-            <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
-              <Image src="/edSheeran.jpg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
-              <strong>Photograph</strong>
-              
-              {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
-              <PlayButtonMusics />
-            </a>
-              
-          </div>
-
-          {/* Texto para a SEGUNDA parte da main */}
-          <h1 className="font-semibold text-3xl mt-10 ml-1 text-zinc-100 hover:underline">Made for Gustavo Abrantes</h1>
-
-          {/* Div para a segunda parte da main */}
-          <div className="grid grid-cols-5 gap-6 mt-8">
-            <a href="/Daily1" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3">
-              <Image src="/daily1.jpeg" className="w-full rounded-lg" width={90} height={60} alt="Capa do mix 1" />
-              <strong className="font-semibold">Daily Mix 1</strong>
-              <span className="text-xs text-zinc-400">Lana Del Rey, COIN, girl in red and more</span>
-
-              {/* Fazendo importação do PlayButton */}
-              <PlayButtonPlaylists/>
-            </a>
-
-            <a href="/Daily2" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3">
-              <Image src="/daily2.jpeg" className="w-full rounded-lg" width={90} height={60} alt="Capa do mix 1" />
-              <strong className="font-semibold">Daily Mix 2</strong>
-              <span className="text-xs text-zinc-400">Wallows, COIN, playlists by spotify and more</span>
-
-              {/* Fazendo importação do PlayButton */}
-              <PlayButtonPlaylists />
-            </a>
-
-            <a href="/Daily3" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3">
-              <Image src="/daily3.jpeg" className="w-full rounded-lg" width={90} height={60} alt="Capa do mix 1" />
-              <strong className="font-semibold">Daily Mix 3</strong>
-              <span className="text-xs text-zinc-400">Wallows, COIN, girl in red and more</span>
-
-              {/* Fazendo importação do PlayButton */}
-              <PlayButtonPlaylists />
-            </a>
-
-            <a href="/Daily4" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3">
-              <Image src="/daily4.jpeg" className="w-full rounded-lg" width={90} height={60} alt="Capa do mix 1" />
-              <strong className="font-semibold">Daily Mix 4</strong>
-              <span className="text-xs text-zinc-400">The Weeknd, COIN, Only in spotify</span>
-
-              {/* Fazendo importação do PlayButton */}
-              <PlayButtonPlaylists />
-            </a>
-
-            <a href="/Daily5" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3">
-              <Image src="/daily5.jpeg" className="w-full rounded-lg" width={90} height={60} alt="Capa do mix 1" />
-              <strong className="font-semibold">Daily Mix 5</strong>
-              <span className="text-xs text-zinc-400">Bruno Mars, and your bests musics only in spotify</span>
-
-              {/* Fazendo importação do PlayButton */}
-              <PlayButtonPlaylists />
-            </a>
+            {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
+            <PlayButtonMusics />
+          </a>
+          <a href="#" className="bg-slate-50/10 group rounded overflow-hidden flex items-center gap-5 hover:bg-white/25"> 
+            <Image src="/edSheeran.jpg" width={120} height={80} alt="Capa do Albúm Deus e eu no sertão da dupla Victor e Leo" />
+            <strong>Photograph</strong>
             
-          </div>
+            {/* CHAMANDO O COMPONENTE DE PlayButtonMusics */}
+            <PlayButtonMusics />
+          </a>
+            
+        </div>
 
-        </main>
-    );
+        {/* Texto para a SEGUNDA parte da main */}
+        <h1 className="font-semibold text-3xl mt-10 ml-1 text-zinc-100 hover:underline">Made for Gustavo Abrantes</h1>
+
+        {/* Div para a segunda parte da main */}
+        <div className="grid grid-cols-5 gap-6 mt-8">
+          <a href="/Daily1" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3">
+            <Image src="/daily1.jpeg" className="w-full rounded-lg" width={90} height={60} alt="Capa do mix 1" />
+            <strong className="font-semibold">Daily Mix 1</strong>
+            <span className="text-xs text-zinc-400">Lana Del Rey, COIN, girl in red and more</span>
+
+            {/* Fazendo importação do PlayButton */}
+            <PlayButtonPlaylists/>
+          </a>
+
+          <a href="/Daily2" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3">
+            <Image src="/daily2.jpeg" className="w-full rounded-lg" width={90} height={60} alt="Capa do mix 1" />
+            <strong className="font-semibold">Daily Mix 2</strong>
+            <span className="text-xs text-zinc-400">Wallows, COIN, playlists by spotify and more</span>
+
+            {/* Fazendo importação do PlayButton */}
+            <PlayButtonPlaylists />
+          </a>
+
+          <a href="/Daily3" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3">
+            <Image src="/daily3.jpeg" className="w-full rounded-lg" width={90} height={60} alt="Capa do mix 1" />
+            <strong className="font-semibold">Daily Mix 3</strong>
+            <span className="text-xs text-zinc-400">Wallows, COIN, girl in red and more</span>
+
+            {/* Fazendo importação do PlayButton */}
+            <PlayButtonPlaylists />
+          </a>
+
+          <a href="/Daily4" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3">
+            <Image src="/daily4.jpeg" className="w-full rounded-lg" width={90} height={60} alt="Capa do mix 1" />
+            <strong className="font-semibold">Daily Mix 4</strong>
+            <span className="text-xs text-zinc-400">The Weeknd, COIN, Only in spotify</span>
+
+            {/* Fazendo importação do PlayButton */}
+            <PlayButtonPlaylists />
+          </a>
+
+          <a href="/Daily5" className="bg-white/5 group relative rounded-lg flex flex-col gap-2 hover:bg-white/10 p-3">
+            <Image src="/daily5.jpeg" className="w-full rounded-lg" width={90} height={60} alt="Capa do mix 1" />
+            <strong className="font-semibold">Daily Mix 5</strong>
+            <span className="text-xs text-zinc-400">Bruno Mars, and your bests musics only in spotify</span>
+
+            {/* Fazendo importação do PlayButton */}
+            <PlayButtonPlaylists />
+          </a>
+          
+        </div>
+
+      </main>
+  );
 }
